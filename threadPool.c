0f1ec529b34b8faee77e90a6e5b92f53c0ad4685
hafala <hafalatech@gmail.com>
2015-05-18 19:58:48 +0300

    WIP 2

--- threadPool.c
+++ threadPool.c
@@ -1,31 +1,61 @@
 #include "threadPool.h"
 
 
+void executer(ThreadPool* pool) { 
+	while(pool->has_destroyed == 0 || pool->shouldWaitForTasks == 0) {  
+		while(osIsQueueEmpty(pool->tasks_queue)) {
+			pthread_cond_wait(&cond, pool->mutex);	//TODO: Check mutex
+		}
+		int res = pthread_mutex_trylock(pool->mutex);
+		if(res != 0) {
+			continue;
+		}
+		TaskNode node = osDequeue(pool->tasks_queue);
+		pthread_mutex_unlock(pool->mutex);
+		(*node.routine)(node.param);
+	}
+}
 
 
 ThreadPool* tpCreate(int numOfThreads) {
 	ThreadPool* new_pool = (ThreadPool*)malloc(sizeof(ThreadPool));
 	new_pool->tasks_queue = osCreateQueue();
 	new_pool->threads =  (pthread_t*)malloc(numOfThreads * sizeof(pthread_t));
+	new_pool->has_destroyed = 0;
+	new_pool->shouldWaitForTasks = 0;
+	pthread_mutex_init(new_pool->mutex , PTHREAD_MUTEX_ERRORCHECK);
+	pthread_cond_init(new_pool->cond, NULL);
 	int i;
 	for(i = 0 ; i < numOfThreads ; i++){
-		pthread_create(threads + i, NULL, );		//TODO: Complete
+		pthread_create(threads + i, NULL, &executer, new_pool);		
 	}
 	return new_pool;
 }
 
 
 void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks) {
-	if(shouldWaitForTasks == 0){
-		
-	} else { 
+	threadPool->has_destroyed = 1;
+	threadPool->shouldWaitForTasks = shouldWaitForTasks;
 	
+	//TOD0: WAIT FOR THREADS
+	
+	while(!osIsQueueEmpty(threadPool->tasks_queue)) {
+		TaskNode* current = osDequeue(threadPool->tasks_queue);
+		free(current);
 	}
+	osDestroyQueue(threadPool->tasks_queue);
+	for(i = 0 ; i < numOfThreads ; i++){
+		pthread_exit(threadPool->threads + i);		
+	}
+	free(threadPool->threads);
+	pthread_mutex_destroy(threadPool->mutex); //what if fails?
+	pthread_cond_destroy(threadPool->cond);
+	free(threadPool);
 }
 
 
 int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *), void* param) {
-
+	if(shouldWaitForTasks != 0); //no insert
 
 
 
--- threadPool.h
+++ threadPool.h
@@ -10,15 +10,23 @@ typedef struct task_node
 	void* (*routine) (void*);
 }TaskNode;
 
+
 typedef struct thread_pool
 {
 	OSQueue* tasks_queue;	// Queue containing all the tasks in the pool   
 	pthread_t* threads;            // Array containing all threads in the pool
+	int has_destroyed;
+	int shouldWaitForTasks;
+	pthread_mutex_t* mutex;
+	pthread_cond_t* cond;  
 }ThreadPool;
+
 
 ThreadPool* tpCreate(int numOfThreads);
 
+
 void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks);
+
 
 int tpInsertTask(ThreadPool* threadPool, void (*computeFunc) (void *), void* param);
 
